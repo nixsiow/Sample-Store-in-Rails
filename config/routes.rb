b26cdeb7b29c1580eb6ae399b1918370caa5a79c
nixsiow <nixsiow@hotmail.com>
2014-10-04 22:26:32 +0800

    shopping cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies
 
@@ -15,7 +17,12 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-    resources :movies, only: [:show, :index]
+  resources :movies, only: [:show, :index]
+
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
 
   # Example resource route with options:
   #   resources :products do
